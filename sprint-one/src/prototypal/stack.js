0c3c4bef9d58e0f978a162023a8fea1fee91db50
whitzhu <whitzhu@gmail.com>
2017-02-08 16:41:10 -0800

    Complete The prototypal pattern stack.js

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+  obj.currentSize = 0;
+  obj.storage = {};
+  return obj;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  size: function() {
+    return this.currentSize;
+  },
+  push: function(value) {
+    this.storage[value] = value;
+    return this.currentSize++;
+  },
+  pop: function() {
+    var array = Object.keys(this.storage); //array is an array of current object keys [a, b]
+    var toDelete = array[array.length - 1];
+    this.currentSize === 0 ? 0 : this.currentSize--;
+    delete this.storage[toDelete];
+    return toDelete;
+  }
+};
 
 
