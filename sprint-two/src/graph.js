3811b88b15656ea506f1a3564bd6cf5f66963604
whitzhu <whitzhu@gmail.com>
2017-02-09 21:40:35 -0800

    Started graph.js... this is incomplete. Erik says we have an issue with the way you are putting object notations in a object property

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,30 +2,63 @@
 
 // Instantiate a new graph
 var Graph = function() {
+//This is pseudoclassical style
+  this.nodes = [];
+  this.edges = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes.push(node);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  if (this.nodes.indexOf(node) === -1) {
+    return false;
+  } else {
+    return true;
+  }
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  if ( this.nodes.indexOf(node) === -1 ) {
+    return;
+  } else {
+    this.nodes.splice(this.nodes.indexOf(node), 1);
+  }
+
+  //assuming node is storing info on it's edge 
+  //reuse removeEdge 
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+
+  if ( this.edges[fromNode] && this.edges[fromNode] === toNode ) {
+    return true;
+  } else if ( this.edges[toNode] && this.edges[toNode] === fromNode ) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  // this.edges.push(JSON.stringify([fromNode, toNode]));
+  this.edges[fromNode] = toNode;
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+
+  if ( this.edges[fromNode] && this.edges[fromNode] === toNode ) {
+    delete this.edges[fromNode];
+  } else if ( this.edges[toNode] && this.edges[toNode] === fromNode ) {
+    delete this.edges[toNode];
+  } 
 };
 
 // Pass in a callback which will be executed on each node of the graph.
